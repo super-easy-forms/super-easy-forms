87b0a7e54dba6aa27b92f9001a9612e6339467a2
gkpty <YOUR@EMAIL.com>
2019-05-19 17:40:08 -0500

    removed tests

--- tests.js
+++
@@ -1,3 +0,0 @@
-const formGenerator = require('./form-generator');
-const invokeUrl = `https://cuckh73lel.execute-api.us-east-1.amazonaws.com/deployment2019-05-19T21-46-45-628Z/`;
-formGenerator.script(invokeUrl);
