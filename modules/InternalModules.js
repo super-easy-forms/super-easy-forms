77ed18ce5b5251e782763a5a7c71e002ecb4669e
gkpty <gkardonski@gmail.com>
2019-12-09 03:51:21 -0500

    dleted validateTemplate from internal modules

--- modules/InternalModules.js
+++ modules/InternalModules.js
@@ -3,7 +3,6 @@
 require('dotenv').config();
 var AWS = require('aws-sdk');
 var fs = require("fs");
-var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15'});
 
 //return a friendly label for the desired value
 function CreateLabel(value){
@@ -70,32 +69,9 @@ function ValidateConfig(profile, options){
   //if(options){}
 }
 
-//check that the created template is good
-function CheckTemplate(formName, templateString){
-  if(!templateString){
-    let rawdata = fs.readFileSync(`forms/${formName}/template.json`);  
-    let obj = JSON.parse(rawdata);
-    var templateString = JSON.stringify(obj);
-  }
-  var params = {
-    TemplateBody: templateString,
-  };
-  cloudformation.validateTemplate(params, function(err, data) {
-    if(err) {
-      console.error(err, err.stack);
-      return false;
-    }
-    else {
-      console.log(data);
-      return true;
-    }
-  });
-}
-
 module.exports = {
   CreateLabel,
   CheckEmail,
   ValidateProfile,
   ValidateConfig,
-  CheckTemplate
 }
