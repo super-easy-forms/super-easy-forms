f2df6bb7aab2809b11713230e606c278760cc18c
gkpty <gkardonski@gmail.com>
2019-12-09 03:48:58 -0500

    fixed callbacks and stuff in the create stack file

--- modules/CreateStack.js
+++ modules/CreateStack.js
@@ -1,15 +1,10 @@
-//Import .env
 require('dotenv').config();
-//package to use the file system
 var fs = require("fs");
-//Import AWS SDK
 var AWS = require('aws-sdk');
-//cloudformation
 var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15'});
 var FormConfig = require('./Config');
 
 module.exports = function CreateStack(formName, templateBody, callback) {
-
   if(!templateBody || typeof templateBody !== "string"){
     if(typeof templateBody === "function"){
       callback = templateBody;
@@ -19,7 +14,7 @@ module.exports = function CreateStack(formName, templateBody, callback) {
       throw new Error(err)
     }
     else {
-      let rawdata = fs.readFileSync(`forms/${formName}/template.json`);  
+      let rawdata = fs.readFileSync(`forms/${formName}/template.json`);
       let templateObject = JSON.parse(rawdata);
       var templateBody = JSON.stringify(templateObject);
     }
@@ -38,7 +33,6 @@ module.exports = function CreateStack(formName, templateBody, callback) {
       console.log(err, err.stack); // an error occurred
     } 
     else {
-      console.log(`Succesfully deployed the stack with ARN ${data.StackId}`); // successful response
       var stackArn = data.StackId;
       FormConfig.AddVar(formName, "stackId", stackArn);
       var params = {
@@ -47,13 +41,12 @@ module.exports = function CreateStack(formName, templateBody, callback) {
       console.log("The Cloudformation Stack is being created . . .")
       cloudformation.waitFor('stackCreateComplete', params, function(err, data) {
         if (err) {
-          console.log(err, err.stack);
+          callback(new Error(err))
         }
         else {
-          console.log(data);
           console.log("the Cloudformation Stack has been created succesfully!")
           if(callback && typeof callback === 'function'){
-            callback();
+            callback(null, stackArn);
           }
         }
       });
