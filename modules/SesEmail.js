608c0a3172fb60b33c1c470b0cd03d7f104e59db
gkpty <gkardonski@gmail.com>
2019-12-08 20:04:06 -0500

    added email validationw regex to email functions

--- modules/SesEmail.js
+++ modules/SesEmail.js
@@ -1,43 +1,70 @@
+require('dotenv').config();
 var AWS = require('aws-sdk');
-//SES
 var ses = new AWS.SES({apiVersion: '2010-12-01'});
-//package to use the file system
 var FormConfig = require('./Config');
 
-function VerifydefaultEmail(senderEmail, callback) {
-  var params = {
-    EmailAddress: senderEmail,
-   };
-  ses.verifyEmailIdentity(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-      return false;
-    }
-    else  {
-      FormConfig.AddSetting("senderEmail", senderEmail)
-      if(callback && typeof callback === 'function'){
-				callback();
-			}
-    }             
-  });
+//validates an email using regex
+function ValidEmail(email){
+  if(/(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(email)){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-//verifies a new email identity to be used with AWS SES
+//verifies a default email to be used with SES and adds it to the settinggs file defaults section
+function VerifydefaultEmail(senderEmail, callback) {
+  if(!ValidEmail(senderEmail)){
+    let err = "Invalid email address."
+    throw new Error(err)
+  }
+  else {
+    var params = {
+      EmailAddress: senderEmail,
+     };
+    ses.verifyEmailIdentity(params, function(err, data) {
+      if (err) {
+        throw new Error(err);
+      }
+      else  {
+        FormConfig.AddSetting("senderEmail", senderEmail);
+        if(callback && typeof callback === 'function'){
+          callback();
+        }
+        else {
+          return 'Success';
+        }
+      }             
+    });
+  }
+}
+
+//verifies a new email identity to be used with AWS SES and adds it form config. if the email hasnt been added before it also adds it to the eamil section.
 function VerifySesEmail(senderEmail, callback) {
-  var params = {
-    EmailAddress: senderEmail,
-   };
-  ses.verifyEmailIdentity(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-      return false;
-    }
-    else  {
-      if(callback && typeof callback === 'function'){
-				callback();
-			}
-    }             
-  });
+  if(!ValidEmail(senderEmail)){
+    let err = "Invalid email address."
+    throw new Error(err)
+  }
+  else {
+    var params = {
+      EmailAddress: senderEmail,
+    };
+    ses.verifyEmailIdentity(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        return false;
+      }
+      else  {
+        if(callback && typeof callback === 'function'){
+          callback();
+        }
+        else{
+          return true
+        }
+      }             
+    });
+  }
 }
 
 //checks that the supplied email vas been verified with AWS
@@ -63,4 +90,10 @@ function ValidateSesEmail(formName, email) {
         }      
       } 
    });
+}
+
+module.exports = {
+  VerifydefaultEmail,
+  VerifySesEmail,
+  ValidateSesEmail,
 }
