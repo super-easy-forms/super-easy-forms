060c5c6a9a36234751cd9903f7878f8ea3eafe57
gkpty <gkardonski@gmail.com>
2019-12-11 17:58:39 -0500

    commented tests and added default test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var fs = require("fs")
-var {optionArgs} = require('./../lib/InternalModules')
-var {CheckForm} = require('./../lib/Config')
-var {createDir} = require('./../build')
+//var {optionArgs} = require('./../lib/InternalModules')
+//var {CheckForm} = require('./../lib/Config')
+//var {createDir} = require('./../build')
 //var AWS = require('aws-sdk-mock');
 
 /*
@@ -26,7 +26,6 @@ describe('Check Form existence', function() {
     });
   });
 });
-*/
 
 describe('Optional Arguments', function() {
   let formName = "testform"
@@ -39,13 +38,12 @@ describe('Optional Arguments', function() {
       })
     });
   });
-
-    /*
-  describe('optional arguments', function() {
+});
+*/
+describe('Super Easy Forms', function() {
+  describe('validate email', function() {
     it('should return false when its not an email', function() {
       assert.equal(/(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test("string"), false);
     });
   });
-  */
 });
-
