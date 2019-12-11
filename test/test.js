ed35d33f2810132d9bf1437f90d71f0d385dc457
gkpty <gkardonski@gmail.com>
2019-12-11 18:55:28 -0500

    added the --exit flag in the test command

--- test/test.js
+++ test/test.js
@@ -13,35 +13,41 @@ describe('Check Form existence', function() {
   let args = {"endpointUrl":"", "formFields":{}}
   //let emptyDir = fs.mkdirSync(`./forms/${formName}`)
   describe('Non existent form', function() {
-    it('Should create a form and a config file for that form', function() {
+    it('Should create a form and a config file for that form', function(done) {
       CheckForm(formName, function(err, data){
         if(err){
           throw new Error(err)
         }
         else{
-          assert.equal(fs.readFileSync(`./forms/${formName}/config.json`, 'utf8'), '{}');
+          fs.readFile(`./forms/${formName}/config.json`, 'utf8', function(err, data){
+            if(err) throw new Error(err)
+            else{
+              assert.equal(data, '{}');
+              done();
+            }
+          })
         }
-      });
+      })
     });
   });
 });
 
 describe('Optional Arguments', function() {
   let formName = "testform"
-  let args = {"endpointUrl":"", "formFields":{}} 
+  let args = {"endpointUrl":"", "formFields":{}}
   describe('Invalid options type', function() {
-    it('should return an error when an invalid options argument is used', function() {
+    it('should return an error when an invalid options argument is used', function(done) {
       optionArgs(formName, args, "invalid param", function(err, data){
-        assert.equal(err.message,"options must be an object with the appropriate keys");
+        if(err) {
+          assert.equal(err.message,"options must be an object with the appropriate keys");
+          done();
+        }
+        else {
+          throw new Error('no error detected');
+        };
       });
     });
   });
 });
 
-describe('Super Easy Forms', function() {
-  describe('validate email', function() {
-    it('should return false when its not an email', function() {
-      assert.equal(/(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test("string"), false);
-    });
-  });
-});
+
