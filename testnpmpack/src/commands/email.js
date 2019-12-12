74c45154130e23ecba4e10acfaeded542df52763
gkpty <gkardonski@gmail.com>
2019-12-12 16:07:02 -0500

    started the email command and fullform command

--- testnpmpack/src/commands/email.js
+++ testnpmpack/src/commands/email.js
@@ -1,20 +1,85 @@
-const {Command, flags} = require('@oclif/command')
+const {Command,flags} = require('@oclif/command')
+const SEF = require('super-easy-forms')
+const open = require('open');
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length;
+}
 
 class EmailCommand extends Command {
-  async run() {
-    const {flags} = this.parse(EmailCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/fargo/Desktop/super_easy_forms/testnpmpack/src/commands/email.js`)
+  static args = [
+    {
+      name: 'form',
+      required: true,
+      description: 'name of the form - must be unique',
+    },
+    {
+      name: 'email',
+      required: true,
+      description: 'the email address that will send the form submission emails',
+    },
+  ]
+  static flags = {
+    recipients: flags.string({
+      char: 'r',                    
+      description: 'email addresses that will recieve form submission emails sepparated by commas',
+      multiple: false,
+      required: false         
+    }), 
+    provider: flags.string({
+      char: 'p',                    
+      description: 'the provider of the email service. SES | GMAIL',
+      multiple: false,
+      required: false,
+      options: ['SES', 'GMAIL'],
+      default: 'SES'    
+    }),
   }
+
+  async run() {
+    const {args, flags} = this.parse(EmailCommand)
+    let options = {endpointUrl:null, formFields:null};
+    let params = {};
+    //validate email
+    //validateSESemail
+    //if valid -> addVar
+    //else -> prompt user -> email verification sent. please confirm your email and press done.
+    if(flags.url){
+      options.endpointUrl = flags.url
+    }
+    if(flags.fields){
+      if(flags.labels){
+        options.formFields = SEF.ParseFields(flags.fields, true);
+      }
+      else {
+        options.formFields = SEF.ParseFields(flags.fields, false);
+      }
+    }
+    Object.keys(options).map(function(key, index) {
+      if(options[key]){
+        params[key] = options[key]
+      }
+    })
+    if(isEmpty(params)){
+      SEF.CreateForm(args.name, function(err, data){
+        if(err) console.error(err)
+        else{
+          open(`forms/${args.name}/${args.name}.html`);
+        }
+      })
+    }
+    else{
+      SEF.CreateForm(args.name, params, function(err, data){
+        if(err) console.error(err)
+        else{
+          open(`forms/${args.name}/${args.name}.html`);
+        }
+      })
+    }
+  } 
 }
 
-EmailCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
-
-EmailCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+EmailCommand.description = `Builds an html form`
 
 module.exports = EmailCommand
+
--- testnpmpack/src/commands/fullform.js
+++ testnpmpack/src/commands/fullform.js
@@ -1,20 +1,79 @@
-const {Command, flags} = require('@oclif/command')
+const {Command,flags} = require('@oclif/command')
+const SEF = require('super-easy-forms')
+const open = require('open');
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length;
+}
 
 class FullformCommand extends Command {
-  async run() {
-    const {flags} = this.parse(FullformCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/fargo/Desktop/super_easy_forms/testnpmpack/src/commands/fullform.js`)
+  static args = [
+    {
+      name: 'name',
+      required: true,
+      description: 'name of the form - must be unique',
+    },
+  ]
+  static flags = {
+      fields: flags.string({
+      char: 'f',                    
+      description: 'Desired form formFields',
+      multiple: false,
+      required: false         
+    }), 
+    labels: flags.boolean({
+      char: 'l',
+      default: false,
+      description: 'Automatically add labels to your form',
+      dependsOn: ['fields']
+    }),
+      url: flags.string({
+      char: 'u',                    
+      description: 'The API endpoint endpointUrl for your form',
+      multiple: false,
+      required: false         
+    }),
   }
+
+  async run() {
+    const {args, flags} = this.parse(FullformCommand)
+    let options = {endpointUrl:null, formFields:null};
+    let params = {};
+    if(flags.url){
+      options.endpointUrl = flags.url
+    }
+    if(flags.fields){
+      if(flags.labels){
+        options.formFields = SEF.ParseFields(flags.fields, true);
+      }
+      else {
+        options.formFields = SEF.ParseFields(flags.fields, false);
+      }
+    }
+    Object.keys(options).map(function(key, index) {
+      if(options[key]){
+        params[key] = options[key]
+      }
+    })
+    if(isEmpty(params)){
+      SEF.CreateForm(args.name, function(err, data){
+        if(err) console.error(err)
+        else{
+          open(`forms/${args.name}/${args.name}.html`);
+        }
+      })
+    }
+    else{
+      SEF.CreateForm(args.name, params, function(err, data){
+        if(err) console.error(err)
+        else{
+          open(`forms/${args.name}/${args.name}.html`);
+        }
+      })
+    }
+  } 
 }
 
-FullformCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
-
-FullformCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+FullformCommand.description = `Builds an html form`
 
 module.exports = FullformCommand
