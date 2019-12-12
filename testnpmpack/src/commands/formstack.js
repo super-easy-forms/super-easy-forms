f3018fe3ab0739a231069c8056dd8f7943b277d8
gkpty <gkardonski@gmail.com>
2019-12-12 06:10:54 -0500

    added listr to formstack

--- testnpmpack/src/commands/formstack.js
+++ testnpmpack/src/commands/formstack.js
@@ -1,20 +1,78 @@
 const {Command, flags} = require('@oclif/command')
+const Listr = require('listr');
+const SEF = require('super-easy-forms')
+const open = require('open');
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length;
+}
 
 class FormstackCommand extends Command {
+  static args = [
+    {
+      name: 'name',
+      required: true,
+      description: 'name of the form - must be unique',
+    },
+  ]
+  static flags = {
+    fields: flags.string({
+      char: 'f',                    
+      description: 'Desired form formFields',
+      multiple: false,
+      required: false         
+    }), 
+    labels: flags.boolean({
+      char: 'l',
+      default: false,
+      description: 'Automatically add labels to your form',
+      dependsOn: ['fields']
+    })
+  }
+
   async run() {
-    const {flags} = this.parse(FormstackCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/fargo/Desktop/super_easy_forms/testnpmpack/src/commands/formstack.js`)
+    const {args, flags} = this.parse(FormstackCommand)
+    let options = {endpointUrl:null, formFields:null};
+    let params = {};
+    const tasks = new Listr([
+      {
+        title: 'Create Form with Stack',
+        task: () => {
+          return new Listr([
+          {
+            title: 'Generating form inputs',
+            task: () => SEF.ParseFields(flags.fields, true)
+          },
+          {
+            title: 'Generating the lambda function',
+            task: () => setTimeout(function(){return 0},2000)
+          },
+          {
+            title: 'Generating the Cloudformation template',
+            task: () => setTimeout(function(){return 0},2000)
+          },
+          {
+            title: 'Deploying the stack',
+            task: () => setTimeout(function(){return 0},2000)
+          },
+          {
+            title: 'Fetching the API enpoint URL',
+            task: () => setTimeout(function(){return 0},2000)
+          },
+          {
+            title: 'Generating the form',
+            task: () => SEF.CreateForm(args.name, params)
+          },
+        ])
+      }
+    }
+    ]);
+    tasks.run().catch(err => {
+      console.error(err);
+    });
   }
 }
 
-FormstackCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
-
-FormstackCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+FormstackCommand.description = `Builds an html form`
 
 module.exports = FormstackCommand
