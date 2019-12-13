980089bf225c6b5269df4c53b093da955e1b190d
gkpty <gkardonski@gmail.com>
2019-12-13 16:59:12 -0500

    added the first test for createform

--- test/createFormTest.js
+++ test/createFormTest.js
@@ -1,54 +1,33 @@
-/*
 var assert = require('assert');
 var fs = require("fs")
 var {optionArgs} = require('./../lib/InternalModules')
 var {CheckForm} = require('./../lib/Config')
 var {createDir} = require('./../build')
+var CreateForm = require('./../lib/CreateForm')
 //var AWS = require('aws-sdk-mock');
 
-describe('Check Form existence', function() {
+describe('Check the createForm method', function() {
   beforeEach(function() {
     createDir("./forms")
   })
   let formName = "testform"
   let args = {"endpointUrl":"", "formFields":{}}
   //let emptyDir = fs.mkdirSync(`./forms/${formName}`)
-  describe('Non existent form', function() {
-    it('Should create a form and a config file for that form', function(done) {
-      CheckForm(formName, function(err, data){
+  describe('No params supplied', function() {
+    it('Should produce an error', function(done) {
+      CreateForm(formName, function(err, data){
         if(err){
-          throw new Error(err)
+          assert.equal(err.message.includes("required parameter"), true);
+          done()
         }
-        else{
-          fs.readFile(`./forms/${formName}/config.json`, 'utf8', function(err, data){
-            if(err) throw new Error(err)
-            else{
-              assert.equal(data, '{}');
-              done();
-            }
-          })
+        else {
+          throw new Error('shouldnt create a form when no options or config params are supplied.')
         }
       })
     });
   });
-});
-
-describe('Optional Arguments', function() {
-  let formName = "testform"
-  let args = {"endpointUrl":"", "formFields":{}}
-  describe('Invalid options type', function() {
-    it('should return an error when an invalid options argument is used', function(done) {
-      optionArgs(formName, args, "invalid param", function(err, data){
-        if(err) {
-          assert.equal(err.message,"options must be an object with the appropriate keys");
-          done();
-        }
-        else {
-          throw new Error('no error detected');
-        };
-      });
-    });
-  });
+  /*
+  let formFields = {"firstName":{"type":"text","label":"","required":true},"lastName":{"type":"text","label":"","required":false},"email":{"type":"email","label":"","required":false},"message":{"type":"textarea","label":"","required":false}}
   describe('Create form from params', function() {
     it('should create a form in ./forms/formName/formName.html', function(done) {
       optionArgs(formName, args, "invalid param", function(err, data){
@@ -75,5 +54,5 @@ describe('Optional Arguments', function() {
       });
     });
   });
+  */
 });
-*/
