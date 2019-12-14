ef80a4a1d58c9c3cd5cec7aed372ffb8a681c7b8
gkpty <gkardonski@gmail.com>
2019-12-14 04:02:16 -0500

    fixed the getSubmissions command and methods

--- testnpmpack/src/commands/submissions.js
+++ testnpmpack/src/commands/submissions.js
@@ -10,14 +10,45 @@ class SubmissionsCommand extends Command {
       description: 'name of the form - must be unique',
     }
   ]
+  /*
+  static flags = {
+    list: flags.boolean({
+      char: 'l',
+      default: true,
+      description: 'Automatically add labels to your form',
+      dependsOn: ['fields']
+    }),
+    export: flags.boolean({
+      char: 'l',
+      default: true,
+      description: 'Automatically add labels to your form',
+      dependsOn: ['fields']         
+    }),
+    format: flags.string({
+      char: 'f',                    
+      description: 'Desired format csv|json',
+      default: 'csv',
+      multiple: false,
+      required: false,
+      dependsOn: ['fields']         
+    }), 
+  }
+  */
   async run() {
     const {args} = this.parse(SubmissionsCommand)
     cli.action.start('Fetching your form submissions')
     SEF.GetSubmissions(args.name, function(err, data){
       if(err) console.error(err)
       else{
+        for(let i = 0; i < data.length; i++){
+          console.log(`${i}.`)
+          let item = data[i];
+          Object.keys(item).map(function(key) {
+            let val = item[key]
+            console.log(`${key}: ${val["S"]}`)
+          }) 
+        }
         cli.action.stop()
-        console.log(JSON.stringify(data))
       }
     })
   }
