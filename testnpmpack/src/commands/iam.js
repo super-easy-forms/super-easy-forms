ff28cab4071820c46cc25da0feb6e55e7fa4f3a6
gkpty <gkardonski@gmail.com>
2019-12-15 03:45:40 -0500

    finished the iam command

--- testnpmpack/src/commands/iam.js
+++ testnpmpack/src/commands/iam.js
@@ -1,20 +1,53 @@
 const {Command, flags} = require('@oclif/command')
+const SEF = require('super-easy-forms')
+const {cli} = require('cli-ux');
+const open = require('open');
 
 class IamCommand extends Command {
-  async run() {
-    const {flags} = this.parse(IamCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/fargo/Desktop/super_easy_forms/testnpmpack/src/commands/iam.js`)
+  static args = [
+    {
+      name: 'user',
+      required: true,
+      description: 'name of the IAM user',
+    },
+    {
+      name: 'region',
+      required: false,
+      description: 'your desired AWS region.',
+    },
+  ]
+  static flags = {
+    create: flags.boolean({
+      char: 'c',
+      default: true,
+      description: 'Helps you create an IAM user and adds its profile to the .env file',
+      exclusive: ['update']
+    }),
   }
+
+  async run() {
+    const {args, flags} = this.parse(IamCommand)
+    let region = null;
+    if(args.region){
+      region = args.region
+    }
+    if(flags.create){
+      cli.action.start('Create your IAM user and hold on to the credentials')
+       SEF.CreateIamUser(args.user, region, function(err, data){
+        if(err) {
+          console.error(err)
+          cli.action.stop('Error')
+        }
+        else{
+          open(data);
+          cli.action.stop()
+          console.log('Then create a new AWS profile in your machine with the name of the IAM user and reference it in your projects .env file')
+        }
+      })
+    }
+  } 
 }
 
-IamCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
-
-IamCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+IamCommand.description = `the --create flag will open up a window with the AWS console so that you confirm the creation of a user with the entered name.`
 
 module.exports = IamCommand
