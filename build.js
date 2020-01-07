af0ef0409f3a69d157ab832511dd14ea80873948
gkpty <gkardonski@gmail.com>
2020-01-07 14:27:49 -0500

    added try catch in build command and edited the createIAMuser method. eliminated addAWSregion and addAWSProfile methods

--- build.js
+++ build.js
@@ -1,4 +1,31 @@
+require('dotenv').config();
 var fs = require("fs");
+var {optionError} = require('./lib/InternalModules')
+
+let regionSet = [
+  "us-east-2",
+  "us-east-1",
+  "us-west-1",
+  "us-west-2",
+  "ap-east-1",
+  "ap-south-1",
+  "ap-northeast-2",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ap-northeast-1",
+  "ca-central-1",
+  "cn-north-1",
+  "cn-northwest-1",
+  "eu-central-1",
+  "eu-west-1",
+  "eu-west-2",
+  "eu-west-3",
+  "eu-north-1",
+  "me-south-1",
+  "sa-east-1",
+  "us-gov-east-1",
+  "us-gov-west-1"
+];
 
 function createFile(file, contents, callback) {
   if(fs.existsSync(file)){
@@ -43,38 +70,55 @@ function createDir(dir, callback){
 }
 
 function initBuild(region, profile, callback){
-  createFile('./settings.json', '{}', function(err, data){
-    if(err) console.log(err)
-    else console.log(data)
-  })
-  createDir('./forms', function(err, data){
-    if(err) console.log(err)
-    else console.log(data)
-  })
   let env_file = "";
-  /* if(region){
-    env_file += `\nAWS_REGION=${region}`
-  }
-  if(profile){
-    env_file += `\nAWS_PROFILE=${profile}`
-  } */
-  createFile('./.env', env_file, function(err, data){
-    if(err) console.log(err)
-    else {
-      console.log(data)
-      createFile('./.gitignore', '.env', function(err, data){
-        if(err) console.log(err)
-        else {
-          if(callback && typeof callback === 'function'){
-            callback(null, data);
-          }
-        }
-      })
+  try {
+    createFile('./settings.json', '{}', function(err, data){
+      if(err) throw new Error(err)
+      else console.log(data)
+    })
+    createDir('./forms', function(err, data){
+      if(err) throw new Error(err)
+      else console.log(data)
+    })
+    if(profile){
+      if(/^[a-zA-Z0-9]*$/.test(profile)){
+        env_file += `\nAWS_PROFILE=${profile}`
+      }
+      else{
+        let err = "AWS Profile invalid. Only alphanumeric characters accepted. No spaces.";
+        throw new Error(err);
+      }
     }
-  });
+    if(region){
+      if(regionSet.includes(region)){
+        env_file += `\nAWS_REGION=${region}`
+      }
+      else {
+        let err = "Invalid AWS region code";
+        throw new Error(err)  
+      }
+    }
+    createFile('./.env', env_file, function(err, data){
+      if(err) throw new Error(err)
+      else {
+        createFile('./.gitignore', '.env', function(err, data){
+          if(err) throw new Error(err)
+          else {
+            if(callback && typeof callback === 'function'){
+              callback(null, data);
+            }
+          }
+        })
+      }
+    });
+  }
+  catch(err){
+    optionError(err, callback)
+  }
 }
 
 module.exports = {
   initBuild,
-  createDir
+  createDir,
+  createFile
 }
